76f8c839b12ccb58e781a9c6339206b4eee4b553
FerEnriquez <feer.hernandez00@gmail.com>
2020-07-03 18:55:44 -0500

    Graphic pie and total part

--- Frontend/coinkeeper/src/components/Balance.js
+++ Frontend/coinkeeper/src/components/Balance.js
@@ -1,10 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { PieChart } from 'react-minimal-pie-chart';
+import '../style/balance.css'
 
 class GeneralBalance extends React.Component{
-    render() {
+		render() {
         return(
-            <h1>Hello!</h1>
+            <div>
+                <div className="total_div">
+                    <h1 className="total_title">Total</h1>
+                    <h1 className="total_number">345,860.20</h1>
+                </div>
+                <div className="chart_div">
+                    <PieChart
+                        data={[
+                            {title: 'One', value: 10, color: '#ff4d4d'},
+                            {title: 'Two', value: 15, color: '#c94479'},
+                            {title: 'Three', value: 20, color: '#c3f241'},
+                            {title: 'Two', value: 15, color: '#0cfc40'},
+                            {title: 'Three', value: 20, color: '#b6cef9'}
+                        ]}
+                    />
+                </div>
+                <div className="table_div">
+                    <table>
+                        <tr>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Age</th>
+                        </tr>
+                        <tr>
+                            <td>Jill</td>
+                            <td>Smith</td>
+                            <td>50</td>
+                        </tr>
+                        <tr>
+                            <td>Eve</td>
+                            <td>Jackson</td>
+                            <td>94</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         );
     }
 }
